331568e9da244d12e9761bd7f148fd3378028fba
Taiwei Ko <152499488+macgeek39@users.noreply.github.com>
2024-02-02 06:43:24 -0600

    Created Calculate function

---
+++ index.js
@@ -0,0 +1,9 @@
+// Compound interest calculator
+
+function calculate(){
+
+    const totalAmount = document.getElementById("total-amount");
+    const principalInput = document.getElementById("principal");
+    const rateInput = document.getElementById("rate");
+    const yearsInput = document.getElementById("years");
+}
